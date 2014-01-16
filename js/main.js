fe2ce9c089fdd4ac6cb7ffa94becbd305d11cd3f
Mark Craig <mark.craig@gmail.com>
2014-01-16 19:24:40 +0100

    Drop AngularJS and Bootstrap in here

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyrighted [year] [name of copyright owner]".
+ *
+ * Copyright 2014 ForgeRock AS
+ */
+
+angular.module('main', ['ngResource', 'ngRoute', 'ui.bootstrap'])
+
+.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/empty.html',
+            controller: function($scope) {
+                $scope.message = 'This page intentionally left blank';
+            }
+        })
+});
