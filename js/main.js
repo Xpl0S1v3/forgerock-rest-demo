f5e89fe9e57e0a38b4357b80f14cbce70ca16c49
Mark Craig <mark.craig@gmail.com>
2014-02-15 09:08:33 +0100

    Add user select drop-down on Read page

--- js/main.js
+++ js/main.js
@@ -14,9 +14,30 @@
  * Copyright 2014 ForgeRock AS
  */
 
-/*global angular */
+/*global angular, window, rsUrl */
+
+/**
+ * The URL to the JSON Resource Servlet.
+ * @type {string} The URL to the JSON Resource Servlet.
+ */
+var rsUrl;
+rsUrl = window.location.protocol + "//" + window.location.hostname + ":" +
+    window.location.port + "/json/";
 
 angular.module('main', ['ngResource', 'ngRoute', 'ui.bootstrap'])
+    .factory('crestResource', function ($resource) {
+        "use strict";
+        var CrestResource;
+
+        CrestResource = $resource("");
+
+        CrestResource.prototype.getAll = function (url, successCb, errorCb) {
+            CrestResource = $resource(url);
+            return CrestResource.get({"_queryFilter": true}, successCb, errorCb);
+        };
+
+        return new CrestResource();
+    })
     .config(function ($routeProvider) {
         "use strict";
 
@@ -28,7 +49,39 @@ angular.module('main', ['ngResource', 'ngRoute', 'ui.bootstrap'])
                 templateUrl: 'partials/create.html'
             })
             .when('/read', {
-                templateUrl: 'partials/read.html'
+                templateUrl: 'partials/read.html',
+                controller: function ($scope, users) {
+                    $scope.users = users;
+                },
+                resolve: {
+                    users: function ($q, crestResource) {
+                        var url, deferred, successCb, options, failureCb;
+
+                        url = rsUrl + "users";
+
+                        deferred = $q.defer();
+
+                        successCb = function (result) {
+                            if (angular.equals(result, {})) {
+                                deferred.reject("No users found");
+                            } else {
+                                options = result.result.sort(function (a, b) {
+                                    return a.fullname[0].toString()
+                                        .localeCompare(b.fullname[0].toString());
+                                });
+                                deferred.resolve(options);
+                            }
+                        };
+
+                        failureCb = function () {
+                            deferred.reject("No users found");
+                        };
+
+                        crestResource.getAll(url, successCb, failureCb);
+
+                        return deferred.promise;
+                    }
+                }
             })
             .when('/update', {
                 templateUrl: 'partials/update.html'
